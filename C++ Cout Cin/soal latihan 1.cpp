61ede9117cc6af7606bb6422f71f4416d6ed2337
Boby Idrus <92260631+BobyIdrus@users.noreply.github.com>
2021-10-10 22:57:35 +0700

    20211010
    20211010

---
+++ C++ Cout Cin/soal latihan 1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string nama;
+    string kelas;
+    string prodi;
+    
+    std::cout << "Soal latihan 1" << std::endl;
+    std::cout << "" << std::endl;
+    
+    std::cout << "Nama \t: ";
+    getline(cin,nama);
+    
+    std::cout << "Kelas \t: ";
+    getline(cin,kelas);
+    
+    std::cout << "Prodi \t: ";
+    getline(cin,prodi);
+}
---
+++ C++ Cout Cin/soal latihan 2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string var_nama;
+    int var_umur, var_tinggi, var_berat;
+    
+    cout << "Soal latihan 2" << endl;
+    cout << "" << endl;
+
+    cout << "Nama "<< var_nama <<"\t: ";
+    getline(cin,var_nama);
+    
+    cout << "Umur "<< var_nama <<" \t: ";
+    cin >> var_umur;
+    
+    cout << "tinggi "<< var_nama <<"\t: ";
+    cin >> var_tinggi;
+    
+    cout << "Berat "<< var_nama <<" \t: ";
+    cin >> var_berat;
+
+}
