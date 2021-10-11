6e7f0a76604c599bec59ccc44b645b529ff9dea1
Boby Idrus <92260631+BobyIdrus@users.noreply.github.com>
2021-10-11 22:34:35 +0700

    Python
    Tambahan Tipe data

---
+++ Py_VAR/Py_Tipe_data.py
@@ -0,0 +1,38 @@
+a = "Hello World"
+print(a)
+print(type(a)) 
+
+print("---------------------")
+
+b = 20
+print(b)
+print(type(b)) 
+
+print("---------------------")
+
+c = 20.5
+print(c)
+print(type(c)) 
+
+print("---------------------")
+
+d = ["apple", "banana", "cherry"]
+print(d)
+print(type(d)) 
+#class 'list'
+
+print("---------------------")
+
+e = ("apple", "banana", "cherry")
+print(e)
+print(type(e)) 
+#class 'tuple'
+
+
+print("---------------------")
+
+f = True
+print(f)
+print(type(f)) 
+#class 'bool'
+
